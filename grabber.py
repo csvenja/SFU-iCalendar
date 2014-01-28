e7e4bbc4c7963d7882dd245a8ed00de36f8d75e3
csvenja <c.svenjax@gmail.com>
2014-01-28 06:09:55 -0800

    Commented

--- grabber.py
+++ grabber.py
@@ -20,45 +20,43 @@ def sfu(username, password):
 		session.post('https://go.sfu.ca/psp/paprd/EMPLOYEE/EMPL/?cmd=login', data=payload)
 		return session
 
+	# student number is needed in extracting frame
 	def get_student_number(session):
 		frame = session.get('https://go.sfu.ca/psp/paprd/EMPLOYEE/EMPL/h/?cmd=getCachedPglt&pageletname=SFU_STU_CENTER_PAGELET&tab=SFU_STUDENT_CENTER&PORTALPARAM_COMPWIDTH=Narrow&ptlayout=N')
 		raw_page = BeautifulSoup(frame.text)
 		student_number = raw_page.find(id='DERIVED_SSS_SCL_EMPLID').string
-		student_name = raw_page.find(id='DERIVED_SSS_SCL_TITLE1$35$').string
+		student_name = raw_page.find(id='DERIVED_SSS_SCL_TITLE1$35$').string # student_name's Student Center
 		student_name = student_name.replace('\'s Student Center', '')
 		return (student_number, student_name)
 	
+	# extract frame
 	def get_frame(session, student_number):
 		frame = session.get('https://sims-prd.sfu.ca/psc/csprd_1/EMPLOYEE/HRMS/c/SA_LEARNER_SERVICES.SS_ES_STUDY_LIST.GBL?Page=SS_ES_STUDY_LIST&Action=U&ACAD_CAREER=UGRD&EMPLID='+student_number+'&INSTITUTION=SFUNV&STRM=1141&TargetFrameName=None')
 		raw_page = BeautifulSoup(frame.text)
 		class_frame = raw_page.find(id="ACE_$ICField68$0")
 		return class_frame
 		
+	# extract text
 	def generate_text(class_frame):
 		soup_all = class_frame.find_all("span")
 		text = []
 		for item in soup_all:
 			if len(item.string.strip()) > 0:
-				text.append(item.string.strip())
+				text.append(item.string.strip()) # remove empty lines
 		return text
 
+	# index class name to separate text for each class
 	def generate_index(class_frame, text):
 		soup_name = class_frame.find_all(title="Class Description")
 		class_index = []
 		for item in soup_name:
 			class_index.append(text.index(item.string))
-		class_count = len(class_index)
-		class_index.append(len(text))
+		class_count = len(class_index) # count class number for loop
+		class_index.append(len(text)) # the last index acts as limit
 		return (class_index, class_count)
 
+	# change weekdays to numbers for datetime.weekday()
 	weekdays = {
-	    'Mon': 'MO',
-	    'Tues': 'TU',
-	    'Wed': 'WE',
-	    'Thurs': 'TH',
-	    'Fri': 'FR',
-	    'Sat': 'SA',
-	    'Sun': 'SU',
 		'MO': 0,
 		'TU': 1,
 		'WE': 2,
@@ -68,87 +66,98 @@ def sfu(username, password):
 		'SU': 6
 	}
 	
+	# parse text to lesson
 	def generate_lesson_item(text_item):
 		lesson_item = {}
-		date_item = []
-		time_item = []
 		lesson_item['start_time'] = text_item[0]
 		lesson_item['end_time'] = text_item[1]
 		lesson_item['days'] = text_item[2].split(',')
 		for day_i, value in enumerate(lesson_item['days']):
-			lesson_item['days'][day_i] = weekdays[value]
-		lesson_item['location'] = text_item[3].replace(u'Location:  ', '')
+			lesson_item['days'][day_i] = value[0:2].upper() # icalendar's byday parameter receive "MO TU WE TH FR SA SU" format input
+		lesson_item['location'] = text_item[3].replace(u'Location:  ', '') # remove redundant caption
 		lesson_item['start_date'] = text_item[4]
 		lesson_item['end_date'] = text_item[5]
-		if text_item[6] == 'Instructor:':
+		if text_item[6] == 'Instructor:': # Final has only 6 lines lesson info
 			lesson_item['instructor'] = text_item[7]
 		return lesson_item
 
+	# parse text to class
 	def generate_class_item(text, class_index, class_i):
 		start = class_index[class_i]
-		end = class_index[class_i+1]
+		end = class_index[class_i + 1]
 		current = start
 		deleted = False
 		class_item = {'lessons':[]}
-		class_item['name'] = text[start].replace('  ', ' ')
-		class_item['section'] = text[start+3]
-		if text[start+4] == 'Section':
+		class_item['name'] = text[start].replace('  ', ' ') # class name has two space between department and number
+		class_item['section'] = text[start + 3]
+		if text[start + 4] == 'Section': # Section classes have no scheduled lesson
 			deleted = True
 			return (deleted, class_item)
 		else:
-			class_item['component'] = text[start+4]
-		class_item['description'] = text[start+5]
-		current = start + 12
-		while current < end - 7:
-			lesson_item = generate_lesson_item(text[current:current+8])
+			class_item['component'] = text[start + 4]
+		class_item['description'] = text[start + 5]
+		current = start + 12 # 12 non-empty lines header for each class
+		while current < end - 7: # 7 captions for class info
+			lesson_item = generate_lesson_item(text[current:current + 8]) # 8 lines for one lesson
 			class_item['lessons'].append(lesson_item)
-			current = current + 9
+			current = current + 9 # move on 9 lines after one lesson
 		return (deleted, class_item)
 
+	# dump class info as a list of dictionary
 	def dump(classes):
 		print json.dumps(classes, ensure_ascii=False, indent=2)
 
+	# parse string to date
 	def datelize(date_string):
-		new_date = datetime.strptime(date_string, '%Y/%m/%d')
+		new_date = datetime.strptime(date_string, '%Y/%m/%d') # e.g. 2014/01/28
 		new_date = new_date.replace(tzinfo=timezone('Canada/Pacific'))
 		return new_date.date()
-		
+
+	# parse string to time
 	def timelize(time_string):
-		new_time = datetime.strptime(time_string, '%I:%M%p')
+		new_time = datetime.strptime(time_string, '%I:%M%p') # e.g. 6:00AM
 		new_time = new_time.replace(tzinfo=timezone('Canada/Pacific'))
 		return new_time.time()
 	
+	# generate ics file
 	def generate_ical():
 		cal = Calendar()
 		cal['version'] = '2.0'
 		cal['prodid'] = '-//Simon Fraser University//Svenja Cao//EN'
 		
-		holidays = [datelize('2014/02/10')]
+		holidays = [datelize('2014/02/10')] # Family day in BC
+		# more to update
 		
 		for class_item in classes:
 			for lesson in class_item['lessons']:
 				start_date = datelize(lesson['start_date'])
 				start_time = timelize(lesson['start_time'])
 				end_time = timelize(lesson['end_time'])
-				start = datetime.combine(start_date, start_time)
-				end = datetime.combine(start_date, end_time)
+				start = datetime.combine(start_date, start_time) # class start datetime
+				end = datetime.combine(start_date, end_time) # class end datetime
 				end_date = datelize(lesson['end_date'])
-				until = datetime.combine(end_date, end_time)
+				until = datetime.combine(end_date, end_time) # recurrence end datetime
 				for day in lesson['days']:
 					event = Event()
 					if lesson['start_date'] == lesson['end_date']:
+						# the lesson with same start and end date is the Final
 						event.add('summary', class_item['name'] + ' Final')
 					else:
 						event.add('summary', class_item['name'] + ' ' + class_item['component'])
 					event.add('dtstart', start)
 					event.add('dtend', end)
 					event.add('rrule', {'freq': 'weekly', 'byday': day, 'until': until, 'wkst': 'SU'})
+					# byday doesn't support list for now
 					event.add('location', lesson['location'])
 					if 'instructor' in lesson:
 						event.add('description', 'Instructor: ' + lesson['instructor'] + '\nSection: ' + class_item['section'])
 					else:
 						event.add('description', 'Section: ' + class_item['section'])
+						# the Final has no instructor
+
 					if start_date.weekday() == weekdays[day]:
+						# if a course has class on first day, the first day won't be ignored
+						# see weekdays{}
 						exdates = []
 					else:
 						exdates = [start]
@@ -160,6 +169,7 @@ def sfu(username, password):
 					cal.add_component(event)
 		return cal.to_ical()
 
+	# main
 	session = login(username, password)
 	student_number, student_name = get_student_number(session)
 	class_frame = get_frame(session, student_number)
@@ -170,11 +180,12 @@ def sfu(username, password):
 	while class_i < class_count:
 		deleted, class_item = generate_class_item(text, class_index, class_i)
 		if not deleted:
+			# delete all 'Section' classes
 			classes.append(class_item)
 		class_i = class_i + 1
 	with open(os.path.join(os.path.dirname(__file__), student_name + '.ics'), 'w') as ical:
 		ical.write(generate_ical())
-#	dump(classes)
+	# dump(classes)
 
 def main():
 	username = raw_input('Username: ')
